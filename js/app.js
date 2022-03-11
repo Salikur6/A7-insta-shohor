d8a96a22889c13ea64be429bbfcfe7966b00f3cc
salikur6 <salikur6@gmail.com>
2022-03-11 19:36:11 +0600

    hide question form love and block history

--- js/app.js
+++ js/app.js
@@ -74,11 +74,13 @@ const switchTab = (id) => {
         document.getElementById("posts").style.display = "grid";
         document.getElementById("liked").style.display = "none";
         document.getElementById("reported").style.display = "none";
+        document.getElementById('quections').style.display='block';
+
     } else if (id === 'liked') {
         document.getElementById("liked").style.display = "block";
         document.getElementById("posts").style.display = "none";
         document.getElementById("reported").style.display = "none";
-
+        document.getElementById('quections').style.display='none';
 
 document.getElementById("liked").innerHTML='';
         displayLikedPosts();
@@ -87,6 +89,7 @@ document.getElementById("liked").innerHTML='';
         document.getElementById("reported").style.display = "block";
         document.getElementById("posts").style.display = "none";
         document.getElementById("liked").style.display = "none";
+        document.getElementById('quections').style.display='none';
 
         document.getElementById("reported").innerHTML='';
         displayReportedPosts();
